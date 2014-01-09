773ebaaaa5fd7b399644123b79eb3946c077e0d5
Bart Lantz <underhilllabs@gmail.com>
2014-01-09 12:35:35 -0700

    Update init-emacs-24.el
    removed old cruft from previous emacs versions

--- init-emacs-24.el
+++ init-emacs-24.el
@@ -15,14 +15,19 @@
 (define-key global-map (kbd "C--") 'text-scale-decrease)
 (define-key global-map (kbd "C-z") 'undo)
 
-;; nano
+;; stuff for nanowrimo
 (define-key global-map "\C-c\C-w" 'word-count)
 (define-key global-map "\C-c\C-r" 'revert-buffer)
 ;; soft-fold lines beyond 90 or so.
 (add-hook 'markdown-mode-hook 'longlines-mode)
 ;; count words in buffer
-(defun word-count nil "Count words in buffer" (interactive)
-(shell-command-on-region (point-min) (point-max) "wc -w"))
+(defun word-count ()
+  "Count words in buffer"
+  (interactive)
+  (shell-command-on-region
+   (point-min)
+   (point-max)
+   "wc -w"))
 
 ;; stop using M-x, stop folding thumb under
 (define-key global-map "\C-c\C-m" 'execute-extended-command)
@@ -64,17 +69,10 @@
 ;   use square brackets for function keys, mouse keys, non-ascii chars
 ;;  (global-set-key [f6] 'line-to-top-of-window)
 
-;; color-mode
-;(add-to-list 'load-path "/usr/share/emacs23/site-lisp/emacs-goodies-el/")
-
 (add-to-list 'load-path "~/.emacs.d/elisp/magit-0.8.2/")
-;;(add-to-list 'load-path "~/.emacs.d/elisp/sass-mode/")
-;;(add-to-list 'load-path "~/.emacs.d/elisp/haml-mode/")
-;;(add-to-list 'load-path "~/.emacs.d/elisp/scss-mode/")
-;;(add-to-list 'load-path "~/.emacs.d/elisp/emacs-color-theme-solarized/")
 (add-to-list 'load-path "/usr/share/emacs22/site-lisp/emacs-goodies-el/")
 (add-to-list 'load-path "~/.emacs.d/elisp/")
-;;(require 'pink-bliss)
+
 ;; turn off opening splash screen
 (setq inhibit-splash-screen t inhibit-startup-echo-area-message t)
 
@@ -83,84 +81,11 @@
 ;;   C-c C-c: commit, P: push
 (require 'magit)
 (global-set-key (kbd "C-x g") 'magit-status)
-;;(require 'haml-mode)
-;;(require 'sass-mode)
-;;(require 'scss-mode)
-;;(autoload 'scss-mode "scss-mode")
-;;(add-to-list 'auto-mode-alist '("\\(sass\\|scss\\)$" . scss-mode))
-;;(add-to-list 'auto-mode-alist '("\\.scss\\'" . scss-mode))
 
-;;(load "~/.emacs.d/elisp/nxhtml/autostart.el")
-;; (require 'seamus256)
-;; (require 'color-theme)
-;; (color-theme-initialize)
-;; (require 'zenburn)
-;; (require 'color-theme-solarized)
-
-
-;;(require 'color-theme-tango)
-; (setq color-theme-is-global t)
-;; if emacs is in its own x-window: use robin-hood, else its in a terminal: use the lovely zenburn
-;; (if window-system
-;;     (color-theme-robin-hood)
-;;   (color-theme-seamus256))
-;;(color-theme-katester)
-;;(color-theme-tango)
-;;(color-theme-lethe)  ;; fave 8 color, but looks awful in 256 colors
-;;(color-theme-lawrence)
-;;(color-theme-charcoal-black)
-;;(color-theme-dark-laptop)
-;; always turn on auto-fill/word-wrap in text-mode
-
-;; (setq my-color-themes (list 'color-theme-solarized-dark 'color-theme-solarized-light
-;;                             'color-theme-comidia 'color-theme-robin-hood
-;;                             'color-theme-calm-forest 'color-theme-clarity 'color-theme-goldenrod
-;;                             'color-theme-seamus256 'color-theme-zenburn
-;;                             'color-theme-charcoal-black 'color-theme-dark-laptop))
-
-
-
-;; (defun my-theme-set-default () ; Set the first row
-;;   (interactive)
-;;   (setq theme-current my-color-themes)
-;;   (funcall (car theme-current)))
-
-;; (defun my-describe-theme () ; Show the current theme
-;;   (interactive)
-;;   (message "%s" (car theme-current)))
-
-; Set the next theme (fixed by Chris Webber - tanks)
-;; (defun my-theme-cycle ()
-;;   (interactive)
-;;   (setq theme-current (cdr theme-current))
-;;   (if (null theme-current)
-;;       (setq theme-current my-color-themes))
-;;   (funcall (car theme-current))
-;;   (message "%S" (car theme-current)))
-
-; (setq theme-current my-color-themes)
-; (setq color-theme-is-global nil) ; Initialization
-; (my-theme-set-default)
-; (global-set-key [f12] 'my-theme-cycle)
-
-
+;; turn on autofill for text-mode
 (add-hook 'text-mode-hook 'turn-on-auto-fill)
 
-
-;; (defun drupal-drush-cc-all (&rest args)
-;;   (interactive)
-;;   (call-process "drush" nil "* drush *" nil "cc" "all"))
-
-
-;; (defun drupal-drush-variable-get (variable)
-;;   (interactive
-;;    (if (and current-prefix-arg (not (consp current-prefix-arg)))
-;;        ""
-;;      (list (read-string "variable: "))))
-;;   (call-process "drush" nil "*Drush*" t "variable-get" variable))
-;; ;
-
-;; php-mode
+;; php-mode and drupal-mode
 (add-to-list 'load-path "~/projects/drupal-mode")
 (require 'drupal-mode)
 (add-to-list 'auto-mode-alist '("\\.\\(module\\|test\\|install\\|theme\\)$" . drupal-mode))
@@ -168,7 +93,6 @@
 (add-to-list 'auto-mode-alist '("\\.info" . conf-mode))
 (add-to-list 'auto-mode-alist '("\\(php\\|inc\\)$" . drupal-mode))
 ;; (add-to-list 'auto-mode-alist '("\\.tpl.php$" . nxhtml-mumamo-mode))
-
 
 ;; I prefer drupal-mode to php-mode
 ;; drupal-mode
@@ -183,7 +107,7 @@
 ;; Save all backup file in this directory, instead of leaving tilde files everywhere
 (setq backup-directory-alist (quote ((".*" . "~/.emacs_backups/"))))
 
-;; make regions visible
+;; Custom Variables
 (custom-set-variables
 
  ;; custom-set-variables was added by Custom.
@@ -196,8 +120,7 @@
 
 (put 'downcase-region 'disabled nil)
 
-;; start each session by opening .emacs file (for now)
-(find-file "~/projects/nanowrimo2013/off-planet-storage.md")
+;; start each session by opening .emacs file (for now) and some other files
 (find-file "~/.emacs")
 (find-file "~/cheatsheet_emacs.txt")
 
@@ -209,24 +132,3 @@
 
 
 (setq css-indent-offset 2)
-;; (custom-set-faces
-;;   ;; custom-set-faces was added by Custom.
-;;   ;; If you edit it by hand, you could mess it up, so be careful.
-;;   ;; Your init file should contain only one such instance.
-;;   ;; If there is more than one, they won't work right.
-;;  '(mumamo-background-chunk-major ((((class color) (min-colors 88) (background dark)) (:background "#232323"))))
-;;  '(mumamo-background-chunk-submode1 ((((class color) (min-colors 88) (background dark)) (:background "#343434"))))
-;;  '(mumamo-background-chunk-submode2 ((((class color) (min-colors 88) (background dark)) (:background "#000000"))))
-;;  '(mumamo-background-chunk-submode3 ((((class color) (min-colors 88) (background dark)) (:background "#4F4F4F"))))
-;;  '(mumamo-background-chunk-submode4 ((((class color) (min-colors 88) (background dark)) (:background "#4F4F4F")))))
-;; (custom-set-faces
-;;  ;; custom-set-faces was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  '(mumamo-background-chunk-major ((((class color) (min-colors 88) (background dark)) (:background "#232323"))) t)
-;;  '(mumamo-background-chunk-submode1 ((((class color) (min-colors 88) (background dark)) (:background "#343434"))) t)
-;;  '(mumamo-background-chunk-submode2 ((((class color) (min-colors 88) (background dark)) (:background "#000000"))) t)
-;;  '(mumamo-background-chunk-submode3 ((((class color) (min-colors 88) (background dark)) (:background "#4F4F4F"))) t)
-;;  '(mumamo-background-chunk-submode4 ((((class color) (min-colors 88) (background dark)) (:background "#4F4F4F"))) t))
-
